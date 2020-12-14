5de3d4b305e27dd4bdde45ba1c8d869716ae7cd2
Fabio Petrone <fa610movic90@gmail.com>
2020-12-14 13:40:44 +0100

    arrotondamento prezzo 2 cifre decimali

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 // alert('elleh');
 var outputContainer = document.getElementById('output');
 
-var km = prompt('Ciao utente.Quanti km vuoi fare oggi?');
+var km = prompt('Ciao utente. Quanti km vuoi fare oggi?');
 var age = prompt('Quanti anni hai?');
 if (!isNaN(km) && !isNaN(age)) {
 var age=parseInt(age);
 var km=parseInt(km);
 if (age<18){
-  var price = 0.8*0.21 * km ;
+  var price = (0.8*0.21*km).toFixed(2) ;
 }
 if(age>65){
-  var price =0.6*0.21*km
+  var price =(0.6*0.21*km).toFixed(2);
 }
-if(age>18 && age<65)
-{var price = 0.21 * km ;}
+if(age>18 && age<65){
+  var price = (0.21*km).toFixed(2);
+}
 
-outputContainer.innerText = price;
+outputContainer.innerText = 'Il prezzo del biglietto è di ' + price + ' €. Trenitalia ti augura buon viaggio!';
 }
 else{
 alert('Devi inserire valori corretti');
