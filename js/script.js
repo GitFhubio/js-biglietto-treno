125932d1a688b02d4545341cd3d9643526cf4a33
Fabio Petrone <fa610movic90@gmail.com>
2020-12-14 14:38:47 +0100

    fringe-mondi alternativi

--- js/script.js
+++ js/script.js
@@ -24,8 +24,6 @@
 // }
 
 
-// fammelo scrivere meglio
-
 // versione 2)
 var outputContainer = document.getElementById('output');
 
@@ -91,7 +89,7 @@ alert('Devi inserire valori corretti');
 //   else {return false;}
 // }
 
-// versione 4
+// versione 4)
 //
 // var outputContainer = document.getElementById('output');
 //
@@ -113,6 +111,28 @@ alert('Devi inserire valori corretti');
 //     case false:
 //     discount=1;
 //   }
+//
+//   var price=(base*discount).toFixed(2);
+//
+//   outputContainer.innerText = 'Il prezzo del biglietto è di ' + price + ' €. Trenitalia ti augura buon viaggio!';
+// }
+// else{
+//   alert('Devi inserire valori corretti');
+// }
+
+// versione 5)
+
+// var outputContainer = document.getElementById('output');
+//
+// var km = prompt('Ciao utente. Quanti km vuoi fare oggi?');
+// var age = prompt('E quanti anni hai?');
+// if (!isNaN(km) && !isNaN(age)) {
+//   var age=parseInt(age);
+//   var km=parseInt(km);
+//
+//   var base=0.21*km;
+//
+// age<18? discount=0.8 : (age>65? discount=0.6 : discount=1);
 //
 //   var price=(base*discount).toFixed(2);
 //
