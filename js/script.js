002d444e12449b21b2461632567872949a2eaf0a
Fabio Petrone <fa610movic90@gmail.com>
2020-12-14 14:26:57 +0100

    prove

--- js/script.js
+++ js/script.js
@@ -34,6 +34,7 @@ var age = prompt('E quanti anni hai?');
 if (!isNaN(km) && !isNaN(age)) {
 var age=parseInt(age);
 var km=parseInt(km);
+
 var base=0.21*km;
 var discount=1;
 
@@ -50,3 +51,73 @@ outputContainer.innerText = 'Il prezzo del biglietto è di ' + price + ' €. Tr
 else{
 alert('Devi inserire valori corretti');
 }
+
+// versione 3)
+// var outputContainer = document.getElementById('output');
+//
+// var km = prompt('Ciao utente. Quanti km vuoi fare oggi?');
+// var age = prompt('E quanti anni hai?');
+// if (!isNaN(km) && !isNaN(age)) {
+// var age=parseInt(age);
+// var km=parseInt(km);
+//
+// var base=0.21*km;
+// var discount=1;
+//
+// if (minorenne(age)){
+//    discount=0.8;
+// }
+// if(anziano(age)){
+//   discount=0.6;
+// }
+// var price=(base*discount).toFixed(2);
+//
+// outputContainer.innerText = 'Il prezzo del biglietto è di ' + price + ' €. Trenitalia ti augura buon viaggio!';
+// }
+// else{
+// alert('Devi inserire valori corretti');
+// }
+//
+// function minorenne(x){
+//   if (x<18) {
+//     return true;
+//   }
+//   else {return false;}
+// }
+// function anziano(x){
+//   if (x>65) {
+//     return true;
+//   }
+//   else {return false;}
+// }
+
+// versione 4
+//
+// var outputContainer = document.getElementById('output');
+//
+// var km = prompt('Ciao utente. Quanti km vuoi fare oggi?');
+// var age = prompt('E quanti anni hai?');
+// if (!isNaN(km) && !isNaN(age)) {
+//   var age=parseInt(age);
+//   var km=parseInt(km);
+//
+//   var base=0.21*km;
+//
+//   switch (age<18) {
+//     case true:
+//     discount=0.8;
+//     case false:
+//     switch(age>65){
+//     case true:
+//     discount=0.6;}
+//     case false:
+//     discount=1;
+//   }
+//
+//   var price=(base*discount).toFixed(2);
+//
+//   outputContainer.innerText = 'Il prezzo del biglietto è di ' + price + ' €. Trenitalia ti augura buon viaggio!';
+// }
+// else{
+//   alert('Devi inserire valori corretti');
+// }
